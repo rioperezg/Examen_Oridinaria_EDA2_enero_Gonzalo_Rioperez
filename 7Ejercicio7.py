cc6b9eaba20a8ab9b4af3079711467fbcb32c8a3
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-05-25 09:42:37 +0200

    modificado ejercicio 1

---
+++ 7Ejercicio7.py
@@ -0,0 +1,7 @@
+"""
+Ash Ketchum, líder del equipo de entrenadores Pokemon, tiene dificultades para transmitir los mensajes al Centro Pokemon, 
+dado que los mismos son muy largos y los satélites espías del Equipo Rocket los interceptan, en un lapso muy corto desde que se 
+transmiten. Por lo cual, nos solicita desarrollar un algoritmo que permita comprimir los mensajes para enviarlos más rápido y no 
+puedan ser interceptados. Contemplando los siguientes requerimientos, implementar un algoritmo que pueda crear un árbol de Huffman a 
+partir de la siguiente tabla y desarrollar las funciones para comprimir y descomprimir un mensaje.
+"""
