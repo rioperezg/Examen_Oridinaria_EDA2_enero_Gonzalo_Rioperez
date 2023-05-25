71f3e22e5131fcad1c40f712bc8fc6d7d6997c67
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-05-25 08:53:03 +0200

    haciendo tests

--- 1Ejercicio1.py
+++ 1Ejercicio1.py
@@ -15,5 +15,26 @@ class Pokemon(object):
         self.nombre = nombre
         self.tipo = tipo
     def __str__(self):
-        return "El pokemon {}, de tipo {} se ha creado con exito"
+        return "El pokemon {}, de tipo {} se ha creado con exito".format(self.nombre, self.tipo)
+    # Ahora pasaremos a crear un metodo clasificacion que clasifique a los pokemon segun su tipo de la la siguiente manera:
+    # los PS, el Ataque, la Defensa, el Ataque Especial, la Defensa Especial y la Velocidad.
+    def clasificacion(self):
+        ps_Pokemon = int(input("Introduzca los Ps del Pokemon: "))
+        Ataque = input("Introduzca el ataque del pokemon: ")
+        Ataque_Esp = input("Introduzca el ataque especial del pokemon: ")
+        Defensa_Esp = input("Introduzca la defensa especial del pokemon: ")
+        Velocidad = input("Introduzca la velocidad del pokemon: ")
+        
+
+
+# Experimentacion
+import unittest
+class TestPokemon(unittest.TestCase):
+    def setUp(self):
+
+       
+
+
+
+
         
