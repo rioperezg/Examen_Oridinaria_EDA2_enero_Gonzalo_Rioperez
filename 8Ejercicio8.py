27ec2902320041e006761b2e8f97cab8556cd3ce
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-05-25 10:01:22 +0200

    implementacion de herramientas necesarias

--- 8Ejercicio8.py
+++ 8Ejercicio8.py
@@ -1,9 +1,14 @@
 """
-Eres uno de los líderes de gimnasio (Gym Leader) de una importante organización Pokémon internacional. Uno de los altos miembros del Alto Mando te informa que el Campeón de la Liga Pokémon ha viajado desde EEUU a Barcelona y ha convocado un torneo de alto nivel para esta tarde a las 19:30 en Barcelona. Han consultado la lista de vuelos desde Madrid (donde estás tú) y han llegado a la conclusión de que tienes que tomar un vuelo que parte de Madrid dentro de 2 horas y cierra su facturación 20 minutos antes.
+Eres uno de los líderes de gimnasio (Gym Leader) de una importante organización Pokémon internacional. Uno de los altos miembros del 
+Alto Mando te informa que el Campeón de la Liga Pokémon ha viajado desde EEUU a Barcelona y ha convocado un torneo de alto nivel para esta 
+tarde a las 19:30 en Barcelona. Han consultado la lista de vuelos desde Madrid (donde estás tú) y han llegado a la conclusión de que tienes 
+que tomar un vuelo que parte de Madrid dentro de 2 horas y cierra su facturación 20 minutos antes.
 
-¿Qué duración mínima tiene la misión? ¿Llega el líder a facturar a tiempo dentro de los 100 minutos disponibles? Si no es así, revisa las dependencias, seguro que estás indicando relaciones basadas en las restricciones de los recursos.
+¿Qué duración mínima tiene la misión? ¿Llega el líder a facturar a tiempo dentro de los 100 minutos disponibles? Si no es así, revisa las 
+dependencias, seguro que estás indicando relaciones basadas en las restricciones de los recursos.
 
-Coloca primero la ruta crítica y sus recursos necesarios. A continuación, coloca y reajusta el resto de las actividades dentro de su holgura y define quién hará qué actividad.
+Coloca primero la ruta crítica y sus recursos necesarios. A continuación, coloca y reajusta el resto de las actividades dentro de su holgura y 
+define quién hará qué actividad.
 
 
 Aquí tienes la tabla generada a partir de los datos proporcionados:
@@ -94,9 +99,35 @@ Viajar al aeropuerto y facturar.
 
  
 
-Ahora que tienes una lista de tareas y sus respectivas duraciones, necesitas decidir el camino más corto para completarlas todas. Considera cada tarea como un nodo y la duración como el peso de la arista que conecta las tareas. Algunas tareas deben completarse antes que otras, lo que implica que los nodos no están todos conectados entre sí.
+Ahora que tienes una lista de tareas y sus respectivas duraciones, necesitas decidir el camino más corto para completarlas todas. 
+Considera cada tarea como un nodo y la duración como el peso de la arista que conecta las tareas. Algunas tareas deben completarse antes 
+que otras, lo que implica que los nodos no están todos conectados entre sí.
 
-Determinar la secuencia óptima de tareas. Recuerda que estos algoritmos son útiles para encontrar el árbol de expansión mínima en un grafo, lo que en este caso representaría la secuencia de tareas con el tiempo total mínimo.
+Determinar la secuencia óptima de tareas. Recuerda que estos algoritmos son útiles para encontrar el árbol de expansión mínima en un grafo, 
+lo que en este caso representaría la secuencia de tareas con el tiempo total mínimo.
 
-¿Cuál algoritmo elegirías en este caso y por qué? Desarrolla y describe el proceso que utilizarías para aplicar este algoritmo al conjunto de tareas.
+¿Cuál algoritmo elegirías en este caso y por qué? Desarrolla y describe el proceso que utilizarías para aplicar este algoritmo al conjunto de 
+tareas.
 """
+# Consideramoscada como un nodo y luego haremos el arbol de expansion minima
+class nodoArista(object):
+    def __init__(self, info , destino):
+        self.info = info
+        self.destino = destino
+        self.sig = None
+class nodoVertice_ferro(object):
+    def __init__(self, info, tipo):
+        self.info = info
+        self.tipo = tipo
+        self.sig = None
+        self.visitado = False
+        self.adyacentes = Arista()
+class Grafo(object):
+    def __init__(self, dirigido = True):
+        self.inicio = None
+        self.dirigido = dirigido
+        self.tamaño = 0
+class Arista(object):
+    def __init__(self):
+        self.inicio = None
+        self.tamaño = 0
