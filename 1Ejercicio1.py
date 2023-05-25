549ef4ea371f24eccdb19cb13d977b73ead5d47c
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-05-25 09:00:57 +0200

    rehaciendo clasificacion

--- 1Ejercicio1.py
+++ 1Ejercicio1.py
@@ -18,20 +18,35 @@ class Pokemon(object):
         return "El pokemon {}, de tipo {} se ha creado con exito".format(self.nombre, self.tipo)
     # Ahora pasaremos a crear un metodo clasificacion que clasifique a los pokemon segun su tipo de la la siguiente manera:
     # los PS, el Ataque, la Defensa, el Ataque Especial, la Defensa Especial y la Velocidad.
-    def clasificacion(self):
-        ps_Pokemon = int(input("Introduzca los Ps del Pokemon: "))
-        Ataque = input("Introduzca el ataque del pokemon: ")
-        Ataque_Esp = input("Introduzca el ataque especial del pokemon: ")
-        Defensa_Esp = input("Introduzca la defensa especial del pokemon: ")
-        Velocidad = input("Introduzca la velocidad del pokemon: ")
+    def clasificacion(self, tipo_ps, tipo_ataque, tipo_ataque_esp, tipo_defensa_esp, tipo_velocidad):
+        self.tipo_ps = tipo_ps
+        self.tipo_ataque = tipo_ataque
+        self.tipo_ataque_esp = tipo_ataque_esp  
+        self.tipo_defensa_esp = tipo_defensa_esp
+        self.tipo_velocidad = tipo_velocidad
+        return "El pokemon {}, de tipo {} tiene {} PS, {} de ataque, {} de ataque especial, {} de defensa especial y {} de velocidad".format(self.nombre, self.tipo, self.tipo_ps, self.tipo_ataque, self.tipo_ataque_esp, self.tipo_defensa_esp, self.tipo_velocidad)
+       
+
+
+# random.sample(list_vehic, 3)
+# Experimentacion
+import unittest, random
+class TestPokemon(unittest.TestCase):
+    lista_pokemon = []
+    def setUp(self):
+        lista_pokemon = []
+        self.pokemon1 = Pokemon("Pikachu", "Electrico")
+        self.pokemon2 = Pokemon("Charmander", "Fuego")
+        self.pokemon3 = Pokemon("Squirtle", "Agua")
+        self.pokemon4 = Pokemon("Bulbasaur", "Planta")
+        self.pokemon5 = Pokemon("Onix", "Tierra")
+        self.pokemon6 = Pokemon("Gastly", "Fantasma")
+        self.pokemon7 = Pokemon("Snorlax", "Normal")
+        lista_pokemon = [self.pokemon1, self.pokemon2, self.pokemon3, self.pokemon4, self.pokemon5, self.pokemon6, self.pokemon7]
+    def test_clasificacion(self, lista_pokemon):
         
 
-
-# Experimentacion
-import unittest
-class TestPokemon(unittest.TestCase):
-    def setUp(self):
-
+            # hAZTE CON TODOS poKEMON
        
 
 
