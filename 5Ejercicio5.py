1ae90ce3bf842d216692744fee862bffca8c0f02
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-05-25 09:37:36 +0200

    pasamos al ejercicio 6

--- 5Ejercicio5.py
+++ 5Ejercicio5.py
@@ -10,3 +10,31 @@ importante para resolver un problema es de qué manera representar la solución,
 Cuando hayas entendido el problema y tengas una solución en mente, desarrolla un algoritmo que permita hallar al menos una solución para 
 distintas cantidades de Pokémon, y luego completa la siguiente tabla.
 """
+
+
+
+
+
+
+
+
+
+# using LinearAlgebra
+
+#La matriz Z es la matriz de adyacencia del grafo. 
+#Para la construcción de la matriz Z lo que hemos hecho es tomar los vértices del grafo
+#y ver como estaban conectadas esas posiciones según los movimientos que pued realizar la ficha del caballo.
+
+Z= [0 0 0 0 0 1 0 1 0 0; 0 0 0 0 0 0 1 0 1 0; 0 0 0 1 0 0 0 1 0 0; 0 0 1 0 0 0 0 0 1 1; 0 0 0 0 0 0 0 0 0 0; 1 0 0 0 0 0 1 0 0 1; 0 1 0 0 0 1 0 0 0 0; 1 0 1 0 0 0 0 0 0 0; 0 1 0 1 0 0 0 0 0 0; 0 0 0 1 0 1 0 0 0 0]
+
+sum(Z^2)
+sum(Z^3)
+sum(Z^4)
+sum(Z^5)
+sum(Z^8)
+sum(Z^10)
+sum(Z^15)
+sum(Z^18)
+sum(Z^21)
+sum(Z^23)
+sum(Z^32)
