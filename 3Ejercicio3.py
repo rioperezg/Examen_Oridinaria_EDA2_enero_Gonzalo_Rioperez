29eca2146fd8e95d5aec7d30d118b89c5dd5324b
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-05-25 09:16:19 +0200

    terminado ejercicio 3

--- 3Ejercicio3.py
+++ 3Ejercicio3.py
@@ -18,4 +18,16 @@ class Pokeball(object):
     def __str__(self):
         return "La pokeball {} pesa {} gramos, cuesta {} euros y se fabrico el {}".format(self.nombre, self.peso, self.precio, self.fecha)
     
-
+# Experimentacion
+import unittest
+class TestPokeball(unittest.TestCase):
+    def setUp(self):
+        self.pokeball1 = Pokeball(200, "Pokeball", 200, "20/04/2018")
+        self.pokeball2 = Pokeball(300, "Superball", 600, "20/04/2018")
+        self.pokeball3 = Pokeball(400, "Ultraball", 1200, "20/04/2018")
+        self.pokeball4 = Pokeball(500, "Masterball", 3000, "20/04/2018")
+    def test_str(self):
+        self.assertEqual(str(self.pokeball1), "La pokeball Pokeball pesa 200 gramos, cuesta 200 euros y se fabrico el 20/04/2018")
+        self.assertEqual(str(self.pokeball2), "La pokeball Superball pesa 300 gramos, cuesta 600 euros y se fabrico el 20/04/2018")
+        self.assertEqual(str(self.pokeball3), "La pokeball Ultraball pesa 400 gramos, cuesta 1200 euros y se fabrico el 20/04/2018")
+        self.assertEqual(str(self.pokeball4), "La pokeball Masterball pesa 500 gramos, cuesta 3000 euros y se fabrico el 20/04/2018")
